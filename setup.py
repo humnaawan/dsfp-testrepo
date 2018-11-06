26c67fdeba2a8e86b70a19333e600704d3d0b1a4
Humna <humna.awan@rutgers.edu>
2018-11-06 15:29:14 -0600

    Added a test package.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='test_package',
+      version='0.1dev',
+      description='Package to test Python packaging.',
+      author='Hunma Awan',
+      author_email='humna.awan@rutgers.edu',
+      packages=['test_package'],
+     ) #complete
